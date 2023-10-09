7086aee7014c043568406b7ce5bf4f217629fe36
Claire Cashmore <clairecashmore2017@gmail.com>
2023-10-09 12:11:52 -0400

    completed Navbar and Links to pages

---
+++ client/src/components/Contact/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { Navigate, Link } from "react-router-dom";
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+    </div>
+  );
+};
+
+export default Contact;

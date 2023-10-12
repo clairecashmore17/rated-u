8807468b46d1c51f48ecd4672af00ad57aebef8d
Claire Cashmore <clairecashmore2017@gmail.com>
2023-10-12 12:51:35 -0400

    small update

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
+import {
+  Toolbar,
+  Box,
+  AppBar,
+  IconButton,
+  Typography,
+  Menu,
+  Container,
+  Avatar,
+  Button,
+  Tooltip,
+  MenuItem,
+} from "@mui/material";
+
 import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
+
 import AdbIcon from "@mui/icons-material/Adb";
 import SchoolIcon from "@mui/icons-material/School";
 import { Link } from "react-router-dom";
@@ -54,9 +58,11 @@ const Navigation = () => {
             href="/"
             sx={{
               mr: 2,
+              m: 2,
               display: { xs: "none", md: "flex" },
               fontFamily: "monospace",
               fontWeight: 700,
+              fontSize: "1.7vw",
               letterSpacing: ".3rem",
               color: "inherit",
               backgroundColor: "#4ba7b8",

1264297ad62d016a08b0b7d8c7bd5cd8c4068856
clairecashmore17 <cbear5@live.com>
2023-10-24 19:05:08 -0400

    added signup functionality

--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -1,11 +1,58 @@
 import React, { useState } from "react";
 import { Box, TextField, Button } from "@mui/material";
+import {useMutation} from "@apollo/client";
+import { ADD_USER } from "../../utils/mutations";
+import Auth from "../../utils/auth";
+
 import "./index.css";
 const Signup = () => {
+  const [userFormData, setUserFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    first_name: "",
+    last_name: ""
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    console.log(event.target);
+    console.log(`name is ${name} value is ${value}`);
+    setUserFormData({ ...userFormData, [name]: value });
+    console.log(userFormData);
+  };
+
+  const [addUser, {error}] = useMutation(ADD_USER);
+  const formHandler = async (event) => {
+    console.log(userFormData);
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    // validation add here
+
+    try{
+      const {data} = await addUser({
+        variables: {...userFormData},
+      });
+      console.log(data);
+
+      Auth.login(data.addUser.token);
+    }catch (e) {
+      console.error(e);
+    }
+
+    setUserFormData({
+      username: "",
+      email: "",
+      password: "",
+      first_name: "",
+      last_name: ""
+    })
+  }
   return (
     <div className="flex-column">
       <p className="head">Please provide your information below:</p>
-      <div className="flex-row">
+      <div className="flex-row" onChange={handleInputChange}>
         <Box
           component="form"
           sx={{
@@ -16,20 +63,23 @@ const Signup = () => {
           }}
           noValidate
           autoComplete="off"
+          
         >
           <TextField
             id="firstName"
+            name="first_name"
             label="First Name"
             variant="outlined"
             size="small"
           />
           <TextField
             id="lastName"
+            name="last_name"
             label="Last Name"
             variant="outlined"
             size="small"
           />
-          <TextField id="email" label="Email" variant="outlined" size="small" />
+          <TextField id="email" name="email" label="Email" variant="outlined" size="small" />
         </Box>
         <Box
           component="form"
@@ -44,12 +94,14 @@ const Signup = () => {
         >
           <TextField
             id="username"
+            name="username"
             label="Username"
             variant="outlined"
             size="small"
           />
           <TextField
             id="password"
+            name="password"
             label="Password"
             variant="outlined"
             size="small"
@@ -64,6 +116,7 @@ const Signup = () => {
           // maxWidth: "45%",
           maxHeight: "50px",
         }}
+        onClick={formHandler}
       >
         Create Account
       </Button>

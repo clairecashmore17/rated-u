e7f18f311e16ae32c9e23a277dc7dc521acd36b4
Natalie Jordan <nrj@gwu.com>
2023-11-03 22:03:41 -0400

    Button Test

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { Navigate, Link } from "react-router-dom";
+import Button from '@mui/material/Button';
 
 const About = () => {
   return (
     <div>
       <h1>About</h1>
+      <Button variant="contained">Contained</Button>
     </div>
   );
 };
